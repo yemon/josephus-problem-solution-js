f6edcac017ad8f8d4ecadc14488380d2adc763a7
Nooray Yemon <yemon@live.com>
2022-01-22 01:56:42 +0600

    updated

--- solution.js
+++ solution.js
@@ -1,8 +1,8 @@
-function josephus_problem_solution_using_bit(n) {
+const josephus_problem_solution_using_bit = (n) {
     let bit = n.toString(2);
     return parseInt(bit.substr(1)+bit[0],2);
 }
 
-function josephus_problem_solution_using_recursive_function(n) {
+const josephus_problem_solution_using_recursive_function = (n) {
   // TODO
 }
